08586fdf726eccdf3c44a0173608e46368d752c3
Fadibagh <fadi2002.ba@gmail.com>
2024-03-25 03:09:41 -0230

    changes for deployment

--- quiz-me-server.py
+++ quiz-me-server.py
@@ -52,4 +52,6 @@ def generate_quiz():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT env var if available, else default to 5000
+    app.run(host='0.0.0.0', port=port)
